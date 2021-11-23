dfead1fffb240df96b8b0bcce4688962a5610fc9
Aifrom <codemusic@mail.ru>
2021-11-23 19:05:40 +0300

    set dynamic badges

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import moment from "moment";
+moment.locale('ru');
 import Calendar from './components/calendar';
 
 export default {
@@ -14,18 +16,43 @@ export default {
   },
   data() {
     return {
-      events: [
-        {id: 1, name: '12:00 - Купить хлеб', date: '2.11.2021', type: 'green'},
-        {id: 2, name: '14:00 - Погулять', date: '17.11.2021', type: 'red'},
-        {id: 3, name: '20:30 - Посмотреть фильм', date: '27.11.2021', type: 'orange'},
-        {id: 4, name: '22:30 - Послушать музыку', date: '27.11.2021', type: 'green'},
-        {id: 5, name: '22:50 - Почитать книгу', date: '27.11.2021', type: 'red'},
-        {id: 6, name: '12:50 - Проверить почту', date: '12.11.2021', type: 'orange'},
-        {id: 7, name: '21:00 - Спорт', date: '20.10.2021', type: 'orange'},
-        {id: 8, name: '10:00 - Покодить', date: '14.12.2021', type: 'orange'},
-        {id: 9, name: '7:33 - Поехать в Москву', date: '31.12.2021', type: 'green'}
-      ]
+      events: [],
     }
+  },
+  created() {
+    const currYear = moment().year()
+    const currMonth = moment().month()
+    let prevMonth
+    let nextMonth
+    let prevYear
+    let nextYear
+    if(currMonth === 0) {
+      prevMonth = 11
+      nextMonth = 1
+      prevYear = currYear - 1
+      nextYear = currYear
+    } else if (currMonth === 11) {
+      prevMonth = 10
+      nextMonth = 0
+      prevYear = currYear
+      nextYear = currYear + 1
+    } else if(currMonth > 0 && currMonth < 11) {
+      prevMonth = currMonth - 1
+      nextMonth = currMonth + 1
+      prevYear = currYear
+      nextYear = currYear
+    }
+    this.events = [
+      {id: 1, name: '12:00 - Купить хлеб', date: moment({year: currYear, month: currMonth, day: 2}).format('L'), type: 'green'},
+      {id: 2, name: '14:00 - Погулять', date: moment({year: currYear, month: currMonth, day: 17}).format('L'), type: 'red'},
+      {id: 3, name: '20:30 - Посмотреть фильм', date: moment({year: currYear, month: currMonth, day: 27}).format('L'), type: 'orange'},
+      {id: 4, name: '22:30 - Послушать музыку', date: moment({year: currYear, month: currMonth, day: 27}).format('L'), type: 'green'},
+      {id: 5, name: '22:50 - Почитать книгу', date: moment({year: currYear, month: currMonth, day: 27}).format('L'), type: 'red'},
+      {id: 6, name: '12:50 - Проверить почту', date: moment({year: currYear, month: currMonth, day: 12}).format('L'), type: 'orange'},
+      {id: 7, name: '21:00 - Спорт', date: moment({year: prevYear, month: prevMonth, day: 20}).format('L'), type: 'orange'},
+      {id: 8, name: '10:00 - Покодить', date: moment({year: nextYear, month: nextMonth, day: 14}).format('L'), type: 'orange'},
+      {id: 9, name: '07:33 - Поехать в Москву', date: moment({year: nextYear, month: nextMonth, day: 24}).format('L'), type: 'green'}
+    ]
   }
 }
 </script>
